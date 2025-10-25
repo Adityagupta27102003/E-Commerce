5b7b2c2ebc66142631b3018841e6c3defbca990b
Aditya Gupta <145580252+Adityagupta27102003@users.noreply.github.com>
2025-10-25 18:46:33 +0530

    Implement CORS with a whitelist of allowed origins
    Added CORS configuration with a whitelist for allowed origins.

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,39 @@ import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
-
-// App Config
 const app = express()
-const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// middlewares
-app.use(express.json())
-app.use(cors())
+const whiteList = [
+    'https://e-commerce-frontend-kohl-mu.vercel.app/',
+    'https://your-custom-domain.com' 
+    
+]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
 
-// api endpoints
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200 
+};
+
+app.use(express.json())
+
+app.use(cors(corsOptions));
+
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
 app.use('/api/order', orderRouter)
 
+
 app.get('/', (req, res) => {
     res.send("API Working")
 })
 
-app.listen(port, () => console.log('Server started on PORT : ' + port))
+export default app;
